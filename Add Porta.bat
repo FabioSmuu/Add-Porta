1352011a47856de38c7c314b4aaa1059dd5b7061
guimaraf <guimaraf@gmail.com>
2021-05-01 12:03:44 -0300

    reduce comments and add information to installer

--- Add Porta.bat
+++ Add Porta.bat
@@ -1,7 +1,6 @@
 @echo off
-color f1
-title Add Portas
 cls
+title Add Portas
 set /p nome=Nome da Porta: 
 cls
 echo.Nome da Porta: %nome%
@@ -12,23 +11,16 @@ echo.Porta: %port%
 echo.
 set /p ok=Pressione Enter caso queira adicionar em seu firewall a porta %port% de nome %nome%.
 cls
+
 echo.Adicionando Porta %port%,"%nome%" UDP de Entrada...
 netsh advfirewall firewall add rule name="%nome%" dir=in action=allow protocol=UDP localport=%port%
-cls
-echo.Adicionando Porta %port%,"%nome%" UDP de Entrada...
 echo.Adicionando Porta %port%,"%nome%" UDP de Saida...
 netsh advfirewall firewall add rule name="%nome%" dir=out action=allow protocol=UDP localport=%port%
-cls
-echo.Adicionando Porta %port%,"%nome%" UDP de Entrada...
-echo.Adicionando Porta %port%,"%nome%" UDP de Saida...
+
 echo.Adicionando Porta %port%,"%nome%" TCP de Entrada...
 netsh advfirewall firewall add rule name="%nome%" dir=in action=allow protocol=TCP localport=%port%
-cls
-echo.Adicionando Porta %port%,"%nome%" UDP de Entrada...
-echo.Adicionando Porta %port%,"%nome%" UDP de Saida...
-echo.Adicionando Porta %port%,"%nome%" TCP de Entrada...
 echo.Adicionando Porta %port%,"%nome%" TCP de Saida...
 netsh advfirewall firewall add rule name="%nome%" dir=out action=allow protocol=TCP localport=%port%
-cls
+
 echo.Porta %port% e Nome "%nome%" TCP/UDP de Entrada e Saida Adicionada.
-exit
+pause
--- README.md
+++ README.md
@@ -3,8 +3,14 @@
 [![N|Solid](https://cdn.discordapp.com/attachments/631607183301148672/724397007170568313/paypal.png)](https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=fabinhoec2210@gmail.com&item_name=F%C3%A1bio&currency_code=BRL)  [![N|Solid](https://cdn.discordapp.com/attachments/631607183301148672/724397005543178270/picpay.png)](https://app.picpay.com/user/smuu)
 
 
-Adiciona portas ao firewall do windows sem muita complica.
-- Para o uso, batas executa-lo como administrador.
+Adiciona portas ao firewall do windows sem muita complicação.
+- Para que o script funcione, execute como administrador
+
+# Clique com o botão direito sobre o firewall ports.bat e escolha "Executar como administrador"
+<img src="/img/fire.png?v=3&s=200" title="" alt="">
+
+Aguarde poucos segundos
 
 **Obrigado pela sua atenção!**
 	
+Limpeza do código feita por Fred Oliveira
